0d87ca9847c76842a6488c0218666c1c9cfc2eee
Julian <lc160518@ll.liemerscollege.nl>
2022-09-26 16:43:58 +0200

    main_channel,create channel, role_selector, participants

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import sys
 import math
 
 import discord
+from discord import app_commands
 from dotenv import load_dotenv
 
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
+intents.message_content = True
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -21,12 +23,39 @@ async def on_ready():
 
 
 @client.event
-async def get_channel():
-    channel = client.get_channel(1021842002591105065)
-    await channel.send("eyo")
+async def on_message(message):
+    if client.user == message.author:
+        return
+    print(message.content)
+    if message.content.startswith("start Weervolven") or message.content.startswith("Start Weervolven"):
+        print(message.content)
+        main_channel = client.get_channel(message.channel.id)
+        await message.channel.send("Dit channel is nu main channel!")
+
 
 @client.event
 async def on_message(message):
-    main_channel = message.channel.id
+    if client.user == message.author:
+        return
+    if message.content.startswith("create channel") or message.content.startswith("Create channel"):
+        test_channel = await message.guild.create_text_channel(name="monkey", reason="test")
+
+
+def role_selector():
+    roles = ["Weerwolf",
+             "Burger",
+             "Ziener",
+             "Heks",
+             "Jager",
+             "Cupido",
+             "Het Onschuldige meisje",
+             "Burgemeester",
+             "Dief"
+             ]
+
+
+def participants():
+    players = []
+
 
 client.run(TOKEN)
